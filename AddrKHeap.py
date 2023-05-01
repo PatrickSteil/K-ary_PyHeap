df332f3bd2a3c6777f774db616a37d7206338d32
Patrick Steil <patricksteil@freenet.de>
2023-05-01 14:14:48 +0200

    Add files via upload

--- AddrKHeap.py
+++ AddrKHeap.py
@@ -1,10 +1,18 @@
-import array
+"""
+Copyright 2023 Patrick Steil
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
 
 class HeapElement(object):
     def __init__(self, key, element_id, index):
-        self.key = key
-        self.element_id = element_id
-        self.index = index
+        self.key = key                  # the key value of this element
+        self.element_id = element_id    # an identifier for this element
+        self.index = index              # the index of this element in the heap
 
     def set_index(self, new_index):
         self.index = new_index
@@ -46,46 +54,55 @@ class HeapElement(object):
         return self.key >= other.get_key()
 
     def __str__(self):
+        # Returns a string representation of this element
         return "HeapElement: Key: " + str(self.key) + " NodeId: " + str(self.element_id)
 
 
 class AddrKHeap(object):
     def __init__(self, k = 4):
         assert k >= 2
-        self.k = k
-        self.heap = []
-        self.size = 0
-        self.element_index = {}
+        self.k = k                      # the k value for this k-ary heap
+        self.heap = []                  # the underlying list to store elements
+        self.size = 0                   # the number of elements in the heap
+        self.element_index = {}         # a dictionary to keep track of element indexes
         
     def empty(self):
+        # Returns True if the heap is empty
         return not self.size
 
     def get_size(self):
+        # Returns the number of elements in the heap
         return self.size
 
     def __parent(self, i):
+        # Returns the index of the parent element for the given index
         return (i - 1) // self.k
 
     def __child(self, i, j):
+        # Returns the index of the j-th child of the element at index i
         return self.k * i + j
 
     def insert(self, element_id, key):
+        # Inserts a new element with the given element_id and key value
         self.heap.append(HeapElement(key, element_id, self.size))
         self.element_index[element_id] = self.size
         self.size += 1
         self.__sift_up(self.size - 1)
 
     def __update_index_of_element(self, index, new_index):
+        # Updates the index of the element at the given index
         e = self.heap[index]
         self.element_index[e.get_element_id()] = new_index;
         e.set_index(new_index)
 
     def __swap(self, i, j):
+        # Swaps the positions of the elements at the given indexes
         self.__update_index_of_element(j, i)
         self.__update_index_of_element(i, j)
         self.heap[j], self.heap[i] = self.heap[i], self.heap[j]
 
     def __sift_up(self, i):
+        # Restores the heap property
         while i > 0:
             parent = self.__parent(i)
             if self.heap[parent] > self.heap[i]:
@@ -95,16 +112,19 @@ class AddrKHeap(object):
                 break
 
     def front_key(self):
+        # Returns the key of the smallest element in the heap
         if (self.empty()):
             return None
         return self.heap[0].get_key()
 
     def front_element_id(self):
+        # Returns the element if of the smallest element in the heap
         if (self.empty()):
             return None
         return self.heap[0].get_element_id()
 
     def extract_min(self):
+        # Returns the smallest element and deletes it from the queue
         if self.size == 0:
             return None
         min_element = self.heap[0]
@@ -120,15 +140,18 @@ class AddrKHeap(object):
         return min_element
 
     def deleteMinNode(self):
+        # Returns the element id of the smallest element and deletes the element from the queue
         return self.extract_min().get_element_id()
 
     def update(self, element_id, new_key):
+        # Calls decreaseKey of inserts the element with the new_key
         if (not self.contains(element_id)):
             self.insert(element_id, new_key)
         else:
             self.decreaseKey(element_id, new_key)
 
     def decreaseKey(self, element_id, new_key):
+        # Updates the key of element_id
         if (not self.contains(element_id)):
             return
 
@@ -138,6 +161,7 @@ class AddrKHeap(object):
         self.__sift_up(index)
 
     def __sift_down(self, i):
+        # Restores the heap property
         while self.__child(i, 1) < self.size:
             min_child = self.__min_child(i)
             if self.heap[i] > self.heap[min_child]:
@@ -147,6 +171,7 @@ class AddrKHeap(object):
                 break
 
     def __min_child(self, i):
+        # Returns the smallest child for the given index
         min_child = self.__child(i, 1)
         for j in range(2, self.k + 1):
             if self.__child(i, j) < self.size and self.heap[self.__child(i, j)] < self.heap[min_child]:
@@ -154,9 +179,11 @@ class AddrKHeap(object):
         return min_child
 
     def contains(self, element_id):
+        # Checks if element is in the heap
         return (element_id in self.element_index)
 
     def remove(self, element_id):
+        # Deletes an element from the heap
         if (not self.contains(element_id) and not self.empty()):
             return
 
@@ -164,6 +191,7 @@ class AddrKHeap(object):
         self.extract_min()
         
     def is_heap(self):
+        # Returns true if the heap fullfills the heap property
         for i in range(1, self.size):
             parent = self.__parent(i)
             if self.heap[parent] > self.heap[i]:
--- AddrKHeapTest.py
+++ AddrKHeapTest.py
@@ -1,16 +1,19 @@
 from AddrKHeap import AddrKHeap
 import random
 
+N = 1000
+
+# Test case 1: Test insert and the heap property
 def test_addrkheap_insert_and_min_heap_property(k):
     heap = AddrKHeap(k)
-    for i in range(1000):
+    for i in range(N):
         heap.insert(i, random.randint(-1000, 1000))
     assert heap.is_heap()
 
 # Test case 2: Test extract_min method and min-heap property
 def test_addrkheap_extract_min_and_min_heap_property(k):
     heap = AddrKHeap(k)
-    elements = [random.randint(-1000, 1000) for i in range(1000)]
+    elements = [random.randint(-1000, 1000) for i in range(N)]
     for i in range(len(elements)):
         heap.insert(i, elements[i])
     sorted_elements = sorted(elements)
@@ -26,7 +29,7 @@ def test_addrkheap_extract_from_empty_heap(k):
 # Test case 4: Test that decreaseKey works properly
 def test_addrkheap_decreaseKey(k):
     heap = AddrKHeap(k)
-    for i in range(1000):
+    for i in range(N):
         heap.insert(i, random.randint(-1000, 1000))
 
     heap.decreaseKey(100, -2000)
