61b49259e241551c892b98b3841f704679cefa7c
Patrick Steil <patrick.steil@student.kit.edu>
2023-05-23 08:55:53 +0200

    Update HeapTest.py

--- HeapTest.py
+++ HeapTest.py
@@ -4,7 +4,7 @@ import random
 # Test case 1: Test insert method and min-heap property
 def test_kheap_insert_and_min_heap_property(k):
     heap = KHeap(k)
-    for i in range(1000):
+    for _ in range(1000):
         heap.insert(random.randint(-1000, 1000))
     assert heap.is_heap()
 
@@ -18,7 +18,7 @@ def test_kheap_insert_many_and_min_heap_property(k):
 # Test case 3: Test extract_min method and min-heap property
 def test_kheap_extract_min_and_min_heap_property(k):
     heap = KHeap(k)
-    elements = [random.randint(-1000, 1000) for i in range(1000)]
+    elements = [random.randint(-1000, 1000) for _ in range(1000)]
     for element in elements:
         heap.insert(element)
     sorted_elements = sorted(elements)
